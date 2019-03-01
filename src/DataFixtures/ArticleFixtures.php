f317826d0e0d57ab9dca748879b00fa5a1028516
Raju Mourya <ramo@live.in>
2019-03-01 20:50:46 +0530

    https://symfonycasts.com/screencast/doctrine-relations/many-to-many-save Saving a ManyToMany Relation + Joins

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -4,10 +4,11 @@ namespace App\DataFixtures;
 
 use App\DataFixtures\BaseFixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use App\Entity\Article;
-use App\Entity\Comment;
+use App\Entity\Tag;
 
-class ArticleFixtures extends BaseFixture
+class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
@@ -56,7 +57,19 @@ EOF
             $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
+
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+            }
         });
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TagFixture::class,
+        ];
     }
 }
