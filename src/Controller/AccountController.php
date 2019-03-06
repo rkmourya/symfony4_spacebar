bbb40e59fc137a5eb5756d6cc94c736c0cd5fbdc
Raju Mourya <ramo@live.in>
2019-03-06 23:20:31 +0530

    https://symfonycasts.com/screencast/symfony-security/fetch-user Fetch the User Object

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Psr\Log\LoggerInterface;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
         return $this->render('account/index.html.twig', [
         ]);
     }
---
+++ src/Controller/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+
+/**
+ * @method User|null getUser()
+ */
+class BaseController extends AbstractController
+{
+}
