73ed0f6e7bf33f2b8e037c35927f27f4153b2f76
Raju Mourya <ramo@live.in>
2019-03-09 13:19:34 +0530

    https://symfonycasts.com/screencast/symfony-security/api-token-entity

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
+use App\Entity\ApiToken;
 
 class UserFixture extends BaseFixture
 {
@@ -17,23 +18,31 @@ class UserFixture extends BaseFixture
 
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(10, 'main_users', function ($i) {
+        $this->createMany(10, 'main_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
             $user->setPassword($this->passwordEncoder->encodePassword($user, 'engage'));
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
             if ($this->faker->boolean) {
                 $user->setTwitterUsername($this->faker->userName);
             }
             return $user;
         });
 
-        $this->createMany(3, 'admin_users', function ($i) {
+        $this->createMany(3, 'admin_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('admin%d@thespacebar.com', $i));
             $user->setFirstName($this->faker->firstName);
             $user->setRoles(['ROLE_ADMIN']);
             $user->setPassword($this->passwordEncoder->encodePassword($user, 'engage'));
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
             if ($this->faker->boolean) {
                 $user->setTwitterUsername($this->faker->userName);
             }
